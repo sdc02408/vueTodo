265b4c2e059032bcc0e4bd4866e697993529eb15
sdc02408 <sdc02408@naver.com>
2022-02-19 02:13:51 +0900

    vue todo

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <section class="todoapp">
+      <Header @insertTodo="insertTodo"/>
+      <Todo :todos="todos"/>
+      <Footer/>
+    </section>
+  </div>
+</template>
+
+<script>
+import "./assets/css/main.css";
+
+import Header from "./components/Header.vue";
+import Todo from "./components/Todo.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Todo,
+    Footer
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: new Date(),
+          text: 'vue study',
+          isDone: true
+        },
+        {
+          id: new Date() + 1,
+          text: 'eat vue',
+          isDone : false
+        }
+    
+      ]
+    }
+  },
+  methods: {
+        insertTodo(text){
+            this.todos = [
+                ...this.todos,
+                {
+                    id: new Date().getTime(),
+                    text,
+                    isDone: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <header class="header">
+        <h1>todos</h1>
+        <input 
+        class="new-todo"
+        v-model="text"
+        @keypress="handleTodo"
+         autofocus autocomplete="off" placeholder="write something"/>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: ""
+        }
+    },
+    methods: {
+        handleTodo({keyCode}) {
+            const {text} = this;
+
+            if(keyCode ===13 && text !=="") {
+                this.$emit("insertTodo", text);
+                this.text= "";
+            }
+        }
+    }
+};
+</script>
+<style>
+h1 {
+  position: absolute;
+  top: -155px;
+  width: 100%;
+  font-size: 100px;
+  font-weight: 100;
+  text-align: center;
+  color: rgba(175, 47, 47, 0.15);
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+}
+</style>
